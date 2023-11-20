0bac31ab9734cdb148529531a5b100c850918801
RamonaMonica <ramonamonica08@icloud.com>
2023-11-20 10:54:02 +0200

    homework 8 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
 
+const form = document.querySelector('.feedback-form'); 
+const emailInput = form.querySelector('input[name="email"]'); 
+const messageInput = form.querySelector('textarea[name="message"]'); 
+
+function saveFormData() { 
+  const formData = { 
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData)); 
+}
+
+function loadFormData() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
+  }
+}
+
+const throttledSave = throttle(saveFormData, 500);
+
+emailInput.addEventListener('input', () => {
+  throttledSave();
+});
+
+messageInput.addEventListener('input', () => {
+  throttledSave();
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.removeItem('feedback-form-state');
+
+  console.log(formData);
+
+  form.reset();
+});
+
+loadFormData();
